3d30af252df7bc552440e67a0f4078fb157f4b4b
Ted Dunning <tdunning@apache.org>
2015-05-16 17:55:11 -0700

    Made times in output a bit more presentable

--- src/main/java/com/tdunning/Freezer.java
+++ src/main/java/com/tdunning/Freezer.java
@@ -1,5 +1,8 @@
 package com.tdunning;
 
+import java.util.Date;
+import java.util.Formatter;
+import java.util.GregorianCalendar;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -13,6 +16,9 @@ import java.util.concurrent.TimeUnit;
  *
  */
 public class Freezer {
+
+    private static final double DT = 0.25;
+
     public static void main(String[] args) {
         Sim s = new Sim();
         Door door = new Door(s);
@@ -20,16 +26,19 @@ public class Freezer {
         TemperatureModel model = new TemperatureModel();
         model.setTemp(TemperatureModel.EXTERNAL_TEMP);
 
-        double time = 0;
-        double step = 0;
-        while (time < TimeUnit.SECONDS.convert(100, TimeUnit.DAYS)) {
+        double t0 = new GregorianCalendar(2015, 6, 1).getTimeInMillis() / 1000.0;
+
+        double time = t0;
+        double step = t0 + DT;
+        while (time < t0 + TimeUnit.SECONDS.convert(100, TimeUnit.DAYS)) {
             double limit = s.nextTransition();
             while (time < limit) {
-                double temp = model.step(door.state, power.state, 0.1);
-                time += 0.1;
-                if (time > step) {
-                    System.out.printf("%.2f\t%.2f\n", time, temp);
-                    step++;
+                double dt = Math.min(step, limit) - time;
+                double temp = model.step(door.state, power.state, dt);
+                time += dt;
+                if (time >= step) {
+                    System.out.printf("%tF %<tT.%03d\t%.2f\t%.2f\n", (long) time * 1000, (long) (1000 * (time - Math.floor(time))), time, temp);
+                    step += DT;
                 }
             }
             s.step();
